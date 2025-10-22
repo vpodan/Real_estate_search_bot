f1c56d6e2177e8a31543bf1dd7a19fb8071e0564
vpoddub <vpod1242@gmail.com>
2025-10-22 19:22:56 +0200

    Remove unnecessary activate_env.ps1 file

--- activate_env.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "Активация виртуального окружения..." -ForegroundColor Green
-& ".\venv\Scripts\Activate.ps1"
-Write-Host "Виртуальное окружение активировано!" -ForegroundColor Green
-Write-Host "Теперь вы можете запускать Python скрипты с установленными библиотеками." -ForegroundColor Yellow
