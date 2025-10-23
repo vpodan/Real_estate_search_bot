bf04f3bcc22d46966e5a17cdbe5fab56009befdd
vpoddub <vpod1242@gmail.com>
2025-10-23 21:52:32 +0200

    Fix2

--- real_estate_embedding_function.py
+++ real_estate_embedding_function.py
@@ -2,7 +2,7 @@
 import os
 from typing import List, Dict, Optional, Tuple
 from langchain_openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
 import re
